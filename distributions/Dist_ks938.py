2d140a0425d4af92ba5aea4bd06b72b57fa63534
kstrittmatter <kastrittmatter@gmail.com>
2020-06-07 21:29:35 -0400

    Update Dist_ks938.py

--- distributions/Dist_ks938.py
+++ distributions/Dist_ks938.py
@@ -2,19 +2,19 @@ import numpy as np
 from .base_distribution import BaseDistribution
 class Dist_ks938(BaseDistribution):
 	def __init__(self):
-		self.x_min = -1
-		self.x_max = 1
-		self.f_max = 1.
+		self.x_min = 0
+		self.x_max = 2.
+		self.f_max = 1.5
 
 
 	def pdf(self, x):
 		"""This is your PDF"""
-		return np.abs(x)
+		return (3./8.)*(x**2.)
 
 	def mean(self):
 		"""This is the mean of the PDF"""
-		return 0.
+		return 0.5
 
 	def std(self):
 		"""This is the standard deviation of the pdf"""
-		return np.sqrt(0.5)
+		return np.sqrt(3./20.)
